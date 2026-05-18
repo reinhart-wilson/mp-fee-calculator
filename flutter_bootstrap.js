99152080027ed6d55a2a6f9d8fe18427e99d6d6b
reinhart-wilson <118143693+reinhart-wilson@users.noreply.github.com>
2026-05-18 13:58:45 +0700

    fix entry point

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "401483157"
+    serviceWorkerVersion: "17308256"
   }
 });
